0542b8d69c1cc718d8217daa7fbe906b1e6bf106
kikluz <savir81@hotmail.com>
2015-08-11 11:19:33 -0700

    add new code for fade in code

--- source/assets/javascripts/components/stickynav.js
+++ source/assets/javascripts/components/stickynav.js
@@ -45,9 +45,9 @@ $(window).on("load",function() {
 
       /* If the object is completely visible in the window, fade it in */
       if (objectBottom < windowBottom) {
-        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+        if ($(this).css('opacity')==0) {$(this).fadeTo(900,3);}
       } else {
-        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+        if ($(this).css('opacity')>=1) {$(this).fadeTo(100,3);}
       }
     });
   }
