e47c2eaf77cd639bb0b47bd52c513a2117b94810
kikluz <savir81@hotmail.com>
2015-08-11 11:35:40 -0700

    putting back fade in set to normal

--- source/assets/javascripts/components/stickynav.js
+++ source/assets/javascripts/components/stickynav.js
@@ -45,9 +45,9 @@ $(window).on("load",function() {
 
       /* If the object is completely visible in the window, fade it in */
       if (objectBottom < windowBottom) {
-        if ($(this).css('opacity')==0) {$(this).fadeTo(20,1);}
+        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
       } else {
-        if ($(this).css('opacity')==1) {$(this).fadeTo(100,2);}
+        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
       }
     });
   }
