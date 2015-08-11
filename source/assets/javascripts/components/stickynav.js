e56248636350ca5cb4bc265c922c2d01b5238665
kikluz <savir81@hotmail.com>
2015-08-11 07:50:09 -0700

    add fade in panels to main page

--- source/assets/javascripts/components/stickynav.js
+++ source/assets/javascripts/components/stickynav.js
@@ -34,3 +34,23 @@ $('nav a').click(function(event) {
     }, 500);
     event.preventDefault();
 });
+
+// Fade In Pannels   http://stackoverflow.com/questions/26694385/fade-in-on-scroll-down-fade-out-on-scroll-up-based-on-element-position-in-win
+$(window).on("load",function() {
+  function fade() {
+    $('.fade').each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+      /* If the object is completely visible in the window, fade it in */
+      if (objectBottom < windowBottom) {
+        if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+      } else {
+        if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }
+  fade(); //Fade in completely visible elements during page-load
+  $(window).scroll(function() {fade();}); //Fade in elements during scroll
+});
